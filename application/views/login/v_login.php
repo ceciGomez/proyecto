13d8e558347e8d713e2f7039c6fd8b99408ad5a3
marcespinoza <marceloespinoza00@gmail.com>
2016-12-21 18:30:33 -0300

    Login
    Logo Sirmi
    Boton arreglado

--- application/views/login/v_login.php
+++ application/views/login/v_login.php
@@ -13,23 +13,23 @@
 </head>
 
 <?php
-$username = array('name' => 'username', 'placeholder' => 'nombre de usuario');
-$password = array('name' => 'password',	'placeholder' => 'introduce tu password');
+$username = array('name' => 'username', 'placeholder' => 'Usuario');
+$password = array('name' => 'password',	'placeholder' => 'Contraseña');
 $submit = array('name' => 'submit', 'value' => 'Iniciar sesión', 'title' => 'Iniciar sesión');
 ?>
 
 <body>
   <div class="login">
-	<h1>SIRMi</h1>
+	<img src="<?php echo base_url('images/logo.png'); ?>" />
 		<?=form_open(base_url().'index.php/c_login/new_user')?>
     <form method="post">
     	<!-- <input type="text" name="u" placeholder="Usuario" required="required" />
         <input type="password" name="p" placeholder="Contraseña" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">Ingresar.</button> -->
 					<?=form_open(base_url().'index.php/c_login/new_user')?>
-					<label for="username">Nombre de usuario:</label>
+					<label for="username">Usuario:</label>
 					<?=form_input($username)?><p><?=form_error('username')?></p>
-					<label for="password">Introduce tu password:</label>
+					<label for="password">Contraseña:</label>
 					<?=form_password($password)?><p><?=form_error('password')?></p>
 					<?=form_hidden('token',$token)?>
 					<button type="submit" class="btn btn-primary btn-block btn-large"><?=form_submit($submit)?></button>
