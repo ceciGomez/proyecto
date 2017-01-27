15230652fbfc3f05c8eec9bb1ba62a2c193af7cb
Cecilia Gomez <cecilia.mlgz@gmail.com>
2017-01-27 19:16:15 -0300

    Algunas esqueletos de vistas de ver-imprimir-editar-ver propietaarios en minutas

---
+++ application/views/escribano/imprimirMinuta.php
@@ -0,0 +1,30 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+   <h1>
+      Imprimir Minuta
+   </h1>
+   <small>Imprimir Minuta</small>
+   <ol class="breadcrumb">
+      <li><a href="<?=base_url()?>index.php/c_loginescri"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Minuta</li>
+   </ol>
+</section>
+<!-- Main content -->
+<section class="content-body">
+<!-- Main row -->
+<div class="row">
+</div>
+</section>
+<!-- /.content-wrapper -->
+<script>
+   $( document ).ready(function() {
+       $('#fechaM').datepicker();
+   });
+    $( document ).ready(function() {
+       $('#fechaPA').datepicker();
+   });
+   
+</script>
+
