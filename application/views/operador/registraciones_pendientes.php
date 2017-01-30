eab4a97f57b1fdce79efe2e676490ba5e128a3a7
wolverine20 <arandapablodamian@gmail.com>
2017-01-30 20:39:06 -0300

    correcciones de registro escribano y algo del operador

---
+++ application/views/operador/registraciones_pendientes.php
@@ -0,0 +1,75 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Operador
+        <small>Bienvenido</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Operador</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+    
+      <!-- Main row -->
+      <div class="row">
+        <!-- Left col -->
+        <section class="col-lg-12 connectedSortable">
+         
+
+          <!-- TO DO List -->
+          <div class="box box-primary">
+            <div class="box-header">
+
+              <h3 class="box-title">Usuarios Pendientes de Aprobación</h3>
+
+              <div class="box-tools pull-right">
+                <ul class="pagination pagination-sm inline">
+                  <li><a href="#">&laquo;</a></li>
+                  <li><a href="#">1</a></li>
+                  <li><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">&raquo;</a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <ul class="todo-list">
+                
+                   <?php  foreach ($esc_pen as $ep){ 
+                      
+                  
+                  echo " <li> $ep->nomyap   <br>                              
+                     <button> Aceptar</button>
+                    <button>Rechazar</button>   
+                        </li>"; } ?>
+            
+                   
+                
+                
+              </ul>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer clearfix no-border">
+              <button type="button" class="btn btn-default pull-right"><i class="fa fa-plus"></i> Add item</button>
+            </div>
+          </div>
+          <!-- /.box -->
+
+       
+
+        </section>
+        <!-- /.Left col -->
+      
+      </div>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
