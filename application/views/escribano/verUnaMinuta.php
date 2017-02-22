9a7970e4b1a2f8556701c9a48331aeefdebfd544
Cecilia Gomez <cecilia.mlgz@gmail.com>
2017-02-21 23:05:05 -0300

    Ver minuta e imprimir - 90% finalizado

--- application/views/escribano/verUnaMinuta.php
+++ application/views/escribano/verUnaMinuta.php
@@ -21,26 +21,66 @@
          </h2>
          <p align="justify">
             Departamento  <strong><?php echo $unEscribano[0]->nombreDpto?>.</strong>
-            - Provincia de <strong><?php echo $unEscribano[0]->nombreProv?>.</strong><br>
-            FUNCIONARIO AUTORIZANTE: Esc: <strong> <?php echo $unEscribano[0]->nomyap?>.</strong><br>
-            <?php foreach ($parcelas as $value) { ?> <br>
-            BIEN: <strong><?php echo $value->descripcion?></strong>.<br><br>
+            - Provincia de <strong><?php echo $unEscribano[0]->nombreProv?>.</strong><br> 
+            <u>FUNCIONARIO AUTORIZANTE: </u> Esc: <strong> <?php echo $unEscribano[0]->nomyap?>.</strong><br><br>
+            <?php foreach ($parcelas as $value) { ?> 
+            <u>NOMENCLATURA CATASTRAL: </u>
+            CIRCUNSCRIPCION: <strong><?php echo $value->circunscripcion ?></strong>
+            SECCION: <strong><?php echo $value->seccion ?></strong>
+            CHACRA: <strong><?php echo $value->chacra?></strong>
+            MANZANA: <strong><?php echo $value->manzana ?></strong>
+            PARCELA: <strong><?php echo $value->parcela ?></strong> <br>
+            <br>
+
+            <u>INSCRIPCION: </u>
+            NRO MATRICULA: <strong> <?php echo $value->nroMatriculaRPI ?></strong>
+            FECHA:  <strong> <?php echo $value->fechaMatriculaRPI ?></strong>
+            TOMO:  <strong> <?php echo $value->tomo ?></strong>
+            FOLIO:  <strong> <?php echo $value->folio ?></strong>
+            FINCA:  <strong> <?php echo $value->finca ?></strong>
+            AÑO:  <strong> <?php echo $value->año ?></strong>
+            <br>
+
+            <u>DESCRIPCION: </u><strong><?php echo $value->descripcion?></strong>.<br><br>
             <?php $data["propietarios"] = $this->M_escribano->getPropietarios($value->idParcela); ?>
             <?php foreach ( $data["propietarios"] as $key) { 
                $nroProp = 0;
                ?>
-            <?php if ($key->tipoPropietario == 'A'): ?> ADQUIRIENTE: <br> <?php endif ?>
-            <?php if  ($key->tipoPropietario == 'T'): ?> TRANSMITENTE: <br><?php endif ?>
-            NRO UF/UC: <strong><?php if ($key->nroUfUc == NULL) echo '-------';  else echo $key->nroUfUc ?> - <?php echo $key->tipoUfUc ?></strong>
-            Fecha de Escritura: <strong><?php if ($key->fechaEscritura == NULL) echo '-------';  else echo $key->fechaEscritura ?>.</strong>
-            Nombre y Apellido: <strong><?php if ($key->titular == NULL) echo '-------';  else echo $key->titular ?>.</strong>
-            Cuit - Cuil: <strong><?php if ($key->cuitCuil == NULL) echo '-------';  else echo $key->cuitCuil ?></strong>
-            Direccion: <strong><?php if ($key->direccion == NULL) echo '-------';  else echo $key->direccion; ?>.</strong>
-            Plano Aprobado: <strong><?php if ($key->planoAprobado == NULL) echo '-------';  else echo $key->planoAprobado ?>.</strong>
-            Fecha de Plano Aprobado: <strong><?php if ($key->fechaPlanoAprobado == NULL) echo '-------';  else echo $key->fechaPlanoAprobado ?>.</strong>
-            Poligonos: <strong><?php if ($key->poligonos == NULL) echo '-------';  else echo $key->poligonos ?>.</strong>
-            Porcentaje de Uf/Uc: <strong><?php if ($key->porcentajeUfUc == NULL) echo '-------';  else echo $key->porcentajeUfUc ?> %.</strong><br>
-            Asentimiento Conyugal: <strong><?php if ($key->conyuge == NULL) echo '-------';  else echo $key->conyuge ?> .</strong>
+            <?php if ($key->tipoPropietario == 'A'): ?> <u> ADQUIRIENTE: </u><?php endif ?>
+            <?php if ($key->tipoPropietario == 'T'): ?> <u>TRANSMITENTE: </u><?php endif ?>
+            
+            <?php if ($key->nroUfUc != NULL): ?>NRO UF/UC: <strong> 
+               <?php echo $key->nroUfUc ?> - <?php echo $key->tipoUfUc; endif?></strong>
+
+            <?php if ($key->fechaEscritura != NULL) : ?>  Fecha de Escritura: <strong> 
+               <?php echo $key->fechaEscritura ; endif?></strong>
+
+            <?php if ($key->titular != NULL): ?>Nombre y Apellido: <strong> 
+               <?php echo $key->titular ; endif?>.</strong>
+
+            <?php if ($key->porcentajeCondominio != NULL): ?>PORCENTAJE DE CONDOMINIO: <strong> 
+               <?php echo $key->porcentajeCondominio; echo '%'; endif?></strong>
+           
+            <?php if ($key->cuitCuil != NULL): ?>Cuit - Cuil: <strong> 
+               <?php echo $key->cuitCuil ; endif?></strong>
+
+            <?php if ($key->direccion != NULL): ?>Direccion: <strong> 
+               <?php echo $key->direccion; endif ?></strong>
+
+            <?php if ($key->planoAprobado != NULL) :?> Plano Aprobado: <strong> 
+               <?php echo $key->planoAprobado ; endif?>.</strong>
+
+            <?php if ($key->fechaPlanoAprobado != NULL): ?>Fecha de Plano Aprobado: <strong>
+               <?php echo $key->fechaPlanoAprobado ; endif?></strong>
+
+            <?php if ($key->poligonos != NULL) : ?>Poligonos: <strong>
+               <?php echo $key->poligonos ; endif?></strong>
+
+            <?php if ($key->porcentajeUfUc != NULL):?>Porcentaje de Uf/Uc: <strong> 
+               <?php echo $key->porcentajeUfUc; echo '%' ; endif?></strong><br>
+
+            <?php if ($key->conyuge != NULL):?>Asentimiento Conyugal: <strong> 
+               <?php echo $key->conyuge ; endif?> </strong>
             <br>
             <?php  } ?>
             <?php  } ?>
@@ -48,8 +88,8 @@
       </article>
       <div class="row col-sm-12">
          <a class="btn btn-primary" href="javascript:window.history.back();">Volver</a>
-         <a class="btn btn-primary" href="#" >Imprimir</a>
-         <a class="btn btn-primary" href="<?=base_url()?>index.php/c_loginescri" >Cancelar</a>
+         <a class="btn btn-primary" href="<?=base_url()?>index.php/c_escribano/imprimirMinuta/<?php echo $value->idMinuta?>" >Imprimir</a>
+         
       </div>
       <br>
    </section>
