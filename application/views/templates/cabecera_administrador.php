09f4437124c94348775ee41f1440bfe6917ae838
Ceci <cecilia.mlgz@gmail.com>
2017-02-24 14:21:21 -0300

    Update cabecera_administrador.php

--- application/views/templates/cabecera_administrador.php
+++ application/views/templates/cabecera_administrador.php
@@ -146,7 +146,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?=base_url()?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs"> <?php echo $this->session->userdata('nomyap') ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -154,8 +154,8 @@
                 <img src="<?=base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - [PERFIL]
-                  <small>Miembro desde Nov. 2012</small>
+                  <?php echo $this->session->userdata('nomyap') ?> - [<?php echo $this->session->userdata('perfil') ?>]
+                  <small>Miembro desde  <?php echo $this->session->userdata('fechaReg') ?></small>
                 </p>
               </li>
              
