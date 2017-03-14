31b148aeb41d66ee71cdb3fd0017dcc1576dd329
Cecilia Gomez <cecilia.mlgz@gmail.com>
2017-03-13 23:07:51 -0300

    consulta reporte1

--- application/models/M_reportes.php
+++ application/models/M_reportes.php
@@ -6,20 +6,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  class M_reportes extends CI_Model
  {
  	
- 	public function getMinutasPorFecha($fechaDesde, $fechaHasta)
+ 	public function getMinutasPorFecha($fechaDesde, $fechaHasta, $idEscribano)
  	{
  		try {
  			$query = $this->db->query("
- 				SELECT *
- 				FROM minuta
- 				where fechaIngresoSys between '$fechaDesde' and '$fechaHasta'
+				SELECT m.idMinuta, e.estadoMinuta, e.fechaEstado, e.motivoRechazo, ue.idEscribano, m.fechaIngresoSys, m.fechaEdicion
+					from minuta m inner join estadominuta e on m.idMinuta = e.idMinuta
+					inner join usuarioescribano ue on ue.idEscribano = m.idEscribano
+					where ue.idEscribano = '$idEscribano'
+					and cast(m.fechaIngresoSys as DATE) between '$fechaDesde' and '$fechaHasta'
  				");
  			return $query->reuslt();
  		} catch (Exception $e) {
  			return FALSE;
  		}
  	}
- 	
 	
 	}
  } ?>
