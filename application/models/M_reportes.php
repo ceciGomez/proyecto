e084d943f602f2b2080f050b606413419fdfaf94
Cecilia Gomez <cecilia.mlgz@gmail.com>
2017-03-15 19:09:43 -0300

    consulta de reportes de verMinutas

--- application/models/M_reportes.php
+++ application/models/M_reportes.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				concat(substring(m.fechaIngresoSys, 6, 2), '/' ,substring(m.fechaIngresoSys, 9, 2) , '/', substring(m.fechaIngresoSys, 1, 4)) as fechaIngresoSys,
 				
 				concat(substring(e.fechaEstado, 6, 2), '/' ,substring(e.fechaEstado, 9, 2) , '/', substring(e.fechaEstado, 1, 4)) as fechaEstado,
-				p.circunscripcion, p.seccion, p.chacra, p.quinta, p.fraccion, p.manzana, p.parcela, p.nroMatriculaRPI,
+				p.circunscripcion, p.seccion, p.chacra, p.quinta, p.fraccion, p.manzana, p.parcela, p.nroMatriculaRPI, p.planoAprobado,
 				l.nombre as localidad
 						
 						from minuta m inner join estadominuta e on m.idMinuta = e.idMinuta
